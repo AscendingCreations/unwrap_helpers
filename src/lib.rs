cb236689fd4e9e12d9acd042499530f05174d447
Andrew Wheeler <genusistimelord@gmail.com>
2021-10-29 07:59:03 -0400

    Attempting to allow a direct Calling to the main macro

--- src/lib.rs
+++ src/lib.rs
@@ -15,25 +15,27 @@ macro_rules! unwrap_or_return {
             }
         }
     };
+    ($x:expr, || $expression:expr) => {
+        match $x.to_option() {
+            Some(v) => v,
+            None => {
+                return || $expression();
+            }
+        }
+    };
+    ($x:expr, move || $expression:expr) => {
+        match $x.to_option() {
+            Some(v) => v,
+            None => {
+                return move || $expression();
+            }
+        }
+    };
     ($x:expr, $return:expr) => {
         match $x.to_option() {
             Some(v) => v,
             None => {
                 return $return;
-            }
-        }
-    };
-}
-
-/// Works like `.unwrap`, if it's an Err(_) or None it calls a fn/closure then returns with the result.
-/// Will return No data or Data.
-#[macro_export]
-macro_rules! unwrap_or_fn_return {
-    ($x:expr, $closure:expr) => {
-        match $x.to_option() {
-            Some(v) => v,
-            None => {
-                return $closure();
             }
         }
     };
